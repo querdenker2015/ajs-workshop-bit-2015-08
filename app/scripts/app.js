47ddc65d198567107bdf72077599a61b7c729ff3
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2015-08-06 09:50:41 +0200

    initial ColorPicker

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,19 @@
 "use strict";
 
-angular.module('myApp', []);
+angular.module('myApp', [])
+    .controller('ColorPickerCtrl', function () {
+        this.r = 127;
+        this.g = 255;
+        this.b = 0;
+    })
+    .directive('bitColorPicker', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/colorPicker.html',
+            scope: {
+                r: '@',
+                g: '@',
+                b: '@'
+            }
+        };
+    });
